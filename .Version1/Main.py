f5d30d91550101eb0bc305c017106693fc5e72ed
DanielNowacki2005 <144656552+DanielNowacki2005@users.noreply.github.com>
2023-10-17 22:12:32 +0200

    Update Main.py
    Now with Exit function

--- .Version1/Main.py
+++ .Version1/Main.py
@@ -2,15 +2,19 @@ import Request,Sorting
 import tkinter as tk
 class Main:
     def __init__(self):
-        self.url = self.getUrl()#gets user item
-        self.request = self.searchFull()#gets data
-        if self.request.getData != 1: #if request not failed
-            self.result = self.sort() #sorts data
-            self.show = self.showAsTable() # shows table
-        else:#if request failed
-             print(self.request)
-             print("there is typo or that product dosen't exist")
-
+        self.url = ""
+        while self.url != "https://www.ceneo.pl/exit" :
+            self.url = self.getUrl()#gets user item
+            if self.url == "https://www.ceneo.pl/exit":
+                break
+            else:
+                self.request = self.searchFull()#gets data
+                if self.request.getData != 1: #if request not failed
+                    self.result = self.sort() #sorts data
+                    self.show = self.showAsTable() # shows table
+                else:#if request failed
+                    print(self.request)
+                    print("there is typo or that product dosen't exist")
     def searchFull(self):
         return  Request.Request(self.url)
     def sort(self):
@@ -23,12 +27,10 @@ class Main:
         #headers of the table
         name = tk.Label(window, width=20, fg='black',font=('Arial',16,'bold'),text= self.question+" name.")
         name.grid(row=0, column=1)
-
         price = tk.Label(window, width=20, fg='black',font=('Arial',16,'bold'),text=self.question+" price.")
         price.grid(row=0, column=2)
         productScore = tk.Label(window, width=20, fg='black',font=('Arial',16,'bold'),text=self.question+" Score.")
         productScore.grid(row=0, column=4)
-
         #table body
         totalRows = len(self.result.name)
         for i in range(totalRows-3):
@@ -36,15 +38,12 @@ class Main:
                 self.e = tk.Entry(window, width=50, fg='black',font=('Arial',12,'bold'))
                 self.e.grid(row=i+1, column=1)
                 self.e.insert("end",self.result.name[i+3])
-                
         totalRows = len(self.result.price)
         for i in range(totalRows):
                 if i == self.amount: break
                 self.e = tk.Entry(window, width=10, fg='black',font=('Arial',12,'bold'))
                 self.e.grid(row=i+1, column=2)
                 self.e.insert("end",self.result.price[i])
-                
-
         totalRows = len(self.result.productScore)
         for i in range(totalRows):
                 if i == self.amount: break
@@ -54,7 +53,10 @@ class Main:
                 #writes data
         window.mainloop()
     def getUrl(self):
-        self.question = input("Write what kind of product do you want to search. From Ceneo exapmple komputery for computer or Bizuteria_i_zegarki: ")
+        self.question = input("Write what kind of product do you want to search. From Ceneo exapmple komputery for computer or Bizuteria_i_zegarki or exit to exit: ")
+        url = "https://www.ceneo.pl/"+self.question
+        return url
+aplikacja = Main()
         url = "https://www.ceneo.pl/"+self.question
         return url
 aplikacja = Main()
